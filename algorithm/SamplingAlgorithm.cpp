d628a99b4d60a8b03db0e17a481499f4f884251d
Giacomo Bergami <bergamigiacomo@gmail.com>
2017-11-05 11:22:55 +0100

    Added licence

--- algorithm/SamplingAlgorithm.cpp
+++ algorithm/SamplingAlgorithm.cpp
@@ -1,12 +1,26 @@
-/**
- * SamplingAlgorithm.h
+/*
+ * SamplingAlgorithm.cpp
+ * This file is part of graphSamper
+ *
+ * Copyright (C) 2017 - Giacomo Bergami
  * Created by vasistas on 04/04/17.
+ * 
+ * graphSamper is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * graphSamper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with graphSamper. If not, see <http://www.gnu.org/licenses/>.
  */
 
-//
-// Created by Giacomo Bergami on 04/04/17.
-//
-
+ 
+ 
 #include "SamplingAlgorithm.h"
 #include <fstream>
 
